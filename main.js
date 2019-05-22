f1b387b6d522f76424362cd9f7cf243f28c19aca
Gyorgy Pakozdi <gyorgy.pakozdi@supercharge.io>
2019-05-22 22:21:24 +0200

    sticky footer and stuff

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+const logoSizer = _ => {
+  window.addEventListener("scroll", e => {
+    const mainScreen = document.querySelector(".main").getBoundingClientRect();
+    // console.log(mainScreen.top, mainScreen.height);
+  });
+};
+
+const footerPlacer = _ => {
+  window.addEventListener("scroll", e => {
+    const mainScreen = document.querySelector(".main").getBoundingClientRect();
+    const footer = document.querySelector("footer");
+    const footerHeight = footer.getBoundingClientRect().height;
+    const footerOnScreen = mainScreen.top + footerHeight < 0;
+    footer.style.bottom = `${
+      footerOnScreen ? 0 : 0 - mainScreen.top - footerHeight
+    }px`;
+  });
+};
+
 window.onload = () => {
   writerStart();
+  logoSizer();
+  footerPlacer();
 };
